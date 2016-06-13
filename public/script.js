c08dc6cc1bff911952db75667c162ce9667a8c4e
namester580 <namester182@gmail.com>
2016-06-13 08:05:56 +0200

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,32 +1,43 @@
 /**
  * Created by CoBLiN on 2016/05/23.
  */
- $(document).ready(function(){
+$(document).ready(function(){
+  //  $('#rooms').hide("slide", { direction: "left" }, 1000);
 
+ //   $('#rooms').animate({width: 0});
+  //  $('#rooms').hide();
+  //  $('#rooms').slideDown(1000);
     var voted = false;
     var guessed = false;
     var players = [];
     socket = io();
- //   var room = 'lobby';
-  //  var table = $('#rooms');
-socket.on('connect',function(){
-    voted = false; //=======================================
-    guessed = false;
-    socket.emit('newPlayer',prompt('enter a username'));
+    //   var room = 'lobby';
+    //  var table = $('#rooms');
+
+
+
+
+
+
+
+    socket.on('connect',function(){
+        voted = false; //=======================================
+        guessed = false;
+        socket.emit('newPlayer',prompt('enter a username'));
 //guessed = false;
 
-});
+    });
 
 //.. global
-socket.on('endGame',function(winner){
+    socket.on('endGame',function(winner){
 
-    alert('winner is:' + winner);
+        alert('winner is:' + winner);
 
 
 
-    guessed = false;
-    voted = false;
-});
+        guessed = false;
+        voted = false;
+    });
 
 
 
@@ -34,18 +45,18 @@ socket.on('endGame',function(winner){
 
     $('form').submit(function () {
         // socket.emit('guess', $('#m').val());
-       // if(socket.room!='lobby'){
-      //  alert(socket.room);
-            if (guessed == false ) {
+        // if(socket.room!='lobby'){
+        //  alert(socket.room);
+        if (guessed == false ) {
 
 
-                var guess = $('#m').val();
-                socket.emit('guess', guess);
-                $('#m').val('');
-                guessed = true;
+            var guess = $('#m').val();
+            socket.emit('guess', guess);
+            $('#m').val('');
+            guessed = true;
 
 
-            } else
+        } else
         {
             alert('already guessed');
         }
@@ -65,19 +76,19 @@ socket.on('endGame',function(winner){
     var hAbbrevio = $('h1');
 
 //document.write('<h1 style="text-align: center">' + acro + '<h1>');
-        //   $('li').remove();
-        hAbbrevio.text(abbrevio);
+    //   $('li').remove();
+    hAbbrevio.text(abbrevio);
 //$('h1').innerHTML(acro);
 
-socket.on('chat',function(msg,plr){
-  //  $('li').remove();
- //   $('#players').append('<li id =' + plr +'>' +  plr +':' + msg   + '</li>');
-    $('#msgs').append('<li>' +  plr +':' + msg   + '</li>');
+    socket.on('chat',function(msg,plr){
+         $('#players li').remove();
+        //   $('#players').append('<li id =' + plr +'>' +  plr +':' + msg   + '</li>');
+        $('#msgs').append('<li>' +  plr +':' + msg   + '</li>');
 
-    guessed = false;
+        guessed = false;
 
 
-});
+    });
 
 
 
@@ -86,22 +97,23 @@ socket.on('chat',function(msg,plr){
         hAbbrevio.text(abbrevio);
         if(abbrevio != 'ABBREVIO'){
             $('#msgs li').remove();
-         //   alert('k');
+
+            //   alert('k');
             //$('#rooms li').remove();
-           // $('#players li').remove();
+            // $('#players li').remove();
         }
-        $('#rooms li').remove();
+     //   $('#rooms li').remove();
         $('#players li').remove();
 
-$('.room').remove();
+        $('.room').remove();
         //  alert(socket.id);
         for (var a in rooms) {
-       //    alert(rooms[a].name);
+            //    alert(rooms[a].name);
             // $('#rooms').append('<li><p>' + rooms[a].name +'</p><p>' + rooms[a].abbrevio + '</p><p>' + rooms[a].players.length + '</p><p>' + rooms[a].maxPlayers + '</p>' );
             $('#rooms').append('<tr class = "room" id =' + rooms[a].name + '>' + '<td>' + rooms[a].name + '</td>' + '<td>' + rooms[a].abbrevio + '</td>' + '<td>' + rooms[a].players + '</td>' + '<td>' + rooms[a].maxPlayers + '</td>' + '</tr>');
-        //    $("#abbrevio").html(rooms[a].abbrevio);
+            //    $("#abbrevio").html(rooms[a].abbrevio);
 
-makeClick(rooms[a].name);
+            makeClick(rooms[a].name);
 
 
 
@@ -109,55 +121,61 @@ makeClick(rooms[a].name);
 
         }
 
-for(var e in players){
+        for(var e in players){
 
-    $('#players').append('<li id =' + players[e].name +'>' +  players[e].name +':' +  players[e].guess + '----------------------' + players[e].score   + '</li>');
-
-
-
-    if(players[e].guess != ''){
-        $('#' + players[e].name).click(
+            $('#players').append('<li id =' + players[e].name +'>' +  players[e].name +':' +  players[e].guess + '<span class = "score">' + players[e].score   + '</span></li>');
 
 
-                function() {
-                   //   alert( players[e].name);
-                    if(voted==false) {
-                        vote(players[e].name);
 
-                        voted = true;
-                    }else{
-                        alert('already voted');
+            if(players[e].guess != ''){
+                $('#' + players[e].name).click(
+
+
+                    function() {
+                        //   alert( players[e].name);
+                        if(voted==false) {
+                            vote(players[e].name);
+
+                            voted = true;
+                        }else{
+                            alert('already voted');
+                        }
+
+
+
+
                     }
 
+                );
 
-
-
-                }
-
-        );
-
-    }else{
-        $('#' +players[e].name).click(function(){
-            alert('this user has not geussed yet, cant vote');
-        });
-    }
+            }else{
+                $('#' +players[e].name).click(function(){
+                    alert('this user has not geussed yet, cant vote');
+                });
+            }
 
 
 
 
 
-}
+        }
+        if(abbrevio == 'ABBREVIO'){
+            $('#players li').remove();
+
+
+
+        }
 
     });
     //to make the room elements clickable(workaround)
     function makeClick(el){
         $('#'+el).click(function(){
-           // alert(el);
-if(el!=socket.room){
-    socket.emit('room',el);
-guessed = false;
-    voted = false;
-}
+            // alert(el);
+            if(el!=socket.room){
+                socket.emit('room',el);
+                guessed = false;
+                voted = false;
+            }
 
 
 
@@ -170,4 +188,4 @@ guessed = false;
 
     }
 
- });
+});
